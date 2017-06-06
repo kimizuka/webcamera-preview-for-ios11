9525a7e742fd32ebe5cfe7e6bffb15c87626fea1
kimizuka-fumitaka <f.kimizuka@gmail.com>
2017-06-06 14:10:46 +0900

    :)

---
+++ index.js
@@ -0,0 +1,12 @@
+const medias = {audio : false, video : true},
+      video  = document.getElementById("video");
+
+navigator.getUserMedia(medias, successCallback, errorCallback);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
