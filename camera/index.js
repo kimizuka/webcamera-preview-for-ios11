4d7785c350b03d33222b17e3a4c34a2bbb6f26bd
kimizuka-fumitaka <f.kimizuka@gmail.com>
2017-09-14 11:42:45 +0900

    :)

---
+++ camera/index.js
@@ -0,0 +1,28 @@
+const medias = {audio : false, video : {
+        facingMode : {
+          exact : "environment"
+        }
+      }},
+      video  = document.createElement("video"),
+      canvas = document.getElementById("canvas"),
+      ctx    = canvas.getContext("2d");
+
+navigator.getUserMedia(medias, successCallback, errorCallback);
+
+requestAnimationFrame(draw);
+
+function successCallback(stream) {
+  video.srcObject = stream;
+};
+
+function errorCallback(err) {
+  alert(err);
+};
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  requestAnimationFrame(draw);
+}
