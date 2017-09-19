6908657afda8645b8a92aaff0420679af3f3f4e9
kimizuka-fumitaka <f.kimizuka@gmail.com>
2017-09-19 11:49:12 +0900

    APIつなぎ込み

--- camera/index.js
+++ camera/index.js
@@ -5,13 +5,32 @@ const medias = {audio : false, video : {
       }},
       video  = document.getElementById("video"),
       canvas = document.getElementById("canvas"),
-      ctx    = canvas.getContext("2d");
+      ctx    = canvas.getContext("2d"),
+      txt    = "";
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
 
 canvas.addEventListener("click", () => {
   // let blob = toBlob(canvas);
-  window.open(canvas.toDataURL("image/png"));
+  let base64 = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
+
+  if (!txt) {
+    getTitle(base64).then((evt) => {
+      let result = JSON.parse(evt.target.response);
+
+      return getKnowledge(result.responses[0].labelAnnotations[0].description);
+    }).then((evt) => {
+      let result = JSON.parse(evt.target.response);
+
+      txt = result.itemListElement[0].result.detailedDescription.articleBody;
+    });
+  } else {
+    let msg = new SpeechSynthesisUtterance(txt);
+
+    txt = "";
+    msg.lang = "en-US";
+    speechSynthesis.speak(msg);
+  }
 }, false);
 
 requestAnimationFrame(draw);
@@ -30,6 +49,52 @@ function draw() {
   ctx.drawImage(video, 0, 0);
 
   requestAnimationFrame(draw);
+}
+
+function getTitle(base64) {
+  return new Promise((resolve, reject) => {
+    const URL = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCcdCQ-nMukTK6Kl2xPkrCK6akiG2_o0w4";
+
+    let xhr  = new XMLHttpRequest(),
+        json = {
+          "requests" : [{
+            "image" : {
+              "content" : base64
+            },
+            "features" : [{
+              "type" : "LABEL_DETECTION",
+              "maxResults" : 1
+            }]
+          }]
+        };
+
+    xhr.open("POST", URL);
+    xhr.addEventListener("load", (evt) => {
+      resolve(evt);
+    });
+    xhr.addEventListener("error", () => {
+      reject(evt);
+    });
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(json));
+  });
+}
+
+function getKnowledge(ttl) {
+  return new Promise((resolve, reject) => {
+    const URL = "https://kgsearch.googleapis.com/v1/entities:search?key=AIzaSyCcdCQ-nMukTK6Kl2xPkrCK6akiG2_o0w4";
+
+    let xhr  = new XMLHttpRequest();
+
+    xhr.open("GET", URL + "&query=" + ttl + "&limit=1&indent=true");
+    xhr.addEventListener("load", (evt) => {
+      resolve(evt);
+    });
+    xhr.addEventListener("error", () => {
+      reject(evt);
+    });
+    xhr.send();
+  });
 }
 
 function toBlob(canvas) {
